3a737c49806bebb0021157c3502b649dd009c385
Charlie Ludowici <charlie.ludowici@gmail.com>
2015-12-16 15:27:56 +1100

    faster splitting, removes proper nouns

--- wordStimuliCalculate.py
+++ wordStimuliCalculate.py
@@ -1,9 +1,12 @@
 from __future__ import print_function
 import pandas as pd
 import os
-fname = '6letterWordsFromElexicon'
-words=pd.read_csv(fname+'.csv')
-wordsHalfAsManyLtrs = pd.read_csv('3letterWordsFromElexicon.csv')
+from string import ascii_uppercase
+workDir = os.getcwd()
+sep = os.sep
+fname = '8letterWordsFromElexicon'
+words=pd.read_csv('words_from_databases'+sep+fname+'.csv')
+wordsHalfAsManyLtrs = pd.read_csv('words_from_databases'+sep+'4letterWordsFromElexicon.csv')
 
 #words.to_pickle(fname+'.pickle')
 #words = pd.read_pickle(fname+'.pickle')
@@ -12,44 +15,50 @@ if type(words['Word'].irow(-1)) is not str: #sometimes last row is bad, probably
     
 numLtrs = words['Length'][0] 
 halfNumLtrs = int(numLtrs/2)
-print('words=',words)
+#print('words=',words)
 words['Word'][:halfNumLtrs+1]
-words['firstHalf'] = 'ZZZZZ'
-words['secondHalf'] = 'ZZZZZ'
-#words['firstHalf'][1] = 'N'
-for i in  range( len(words) ):
-    thisWord = words['Word'][i]
-    #print('thisWord=',thisWord)
-    #print('thisWord[:halfNumLtrs]=',thisWord[:3])
-    try:
-        words['firstHalf'][i] = thisWord[:halfNumLtrs]
-    except Exception,e:
-        print(e)
-        print('i=',i)
-    words['secondHalf'][i] = thisWord[halfNumLtrs:]
-#str(words['Word'])
+words['firstHalf'] = words['Word'].str[0:halfNumLtrs]
+words['secondHalf'] = words['Word'].str[halfNumLtrs:numLtrs]
+
+print(words)
+properNouns =[word for word in words['Word'] if  word[0] in ascii_uppercase]
+
+#remove properNouns (or words with initial capital letters) from words
+
+properNounBoolean = words['Word'].isin(properNouns) #single logical column df of len(words) where a cell is True if the word is in properNouns
+
+properNounBooleanInvert = ~properNounBoolean #reverse that, we only want the words that aren't in properNouns
+
+words = words[properNounBooleanInvert]
+words = words.reset_index() #otherwise the indexes have gaps where the properNouns were removed
+
+#words.to_csv(path_or_buf=workDir+sep+'words.csv')
+
+
+
+
 print('words.head=',words.head())
 print('words.tail=',words.tail())
-
+#print(properNouns)
 if type(wordsHalfAsManyLtrs['Word'].irow(-1)) is not str: #sometimes last row is bad, probably because carriage return at end
     wordsHalfAsManyLtrs = wordsHalfAsManyLtrs[:-1]
-print('words3ltrs.head()=',wordsHalfAsManyLtrs)
+#print('words3ltrs.head()=',wordsHalfAsManyLtrs)
 #For each  word, check if both firsthalf and second half is a word
 ngrams = wordsHalfAsManyLtrs['Word'].astype(str)
 ngrams = list(ngrams)
 ngrams = [ x.upper() for x in ngrams ]
-print('ngrams=',ngrams)
+#print('ngrams=',ngrams)
 validWords = []
 validWordsFreq = []
-print('words with both halves legal:')
+#print('words with both halves legal:')
 for i in range(len(words)):
     firstHalf = words['firstHalf'][i].upper()
-    #print('firstHalf=',firstHalf)
+    print('firstHalf=',firstHalf)
     secondHalf = words['secondHalf'][i].upper()
     if i==0:
         print('example firstHalf=',firstHalf,' secondHalf=',secondHalf)
     if firstHalf in ngrams and secondHalf in ngrams:
-        print(words['Word'][i])
+        #print(words['Word'][i])
         validWords.append(firstHalf+secondHalf)
         #validWordFreq.append(words['frequency'][i])
 print(len(validWords),' valid words')
